5db264fcbdf996ced114492e151581b0d7a6d006
xavinso <javiervindas@Javis-MacBook-Pro.local>
2022-06-07 19:24:05 -0600

    Adding a storage stack

--- stacks/index.js
+++ stacks/index.js
@@ -1,4 +1,4 @@
-import { MyStack } from "./MyStack";
+import { StorageStack } from "./StorageStack";
 import { App } from "@serverless-stack/resources";
 
 /**
@@ -12,5 +12,5 @@ export default function (app) {
       format: "esm",
     },
   });
-  app.stack(MyStack);
+  app.stack(StorageStack);
 }
